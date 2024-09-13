8cc30f2afa9b335b24d24d77e06d10e355a30ab3
jota-lang <joaovitoroliver83@gmail.com>
2024-09-13 20:55:09 -0300

    criação de modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import React from "react";
+import { useState } from "react";
 import SideMenu from "./components/sideMenu";
 import Header from "./components/header";
 import Cols from "./components/columns";
+import ModalColumn from "./components/modalColumn";
 
 
 function App() {
-  
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () =>{setIsOpen(true)};
+  const closeModal = () =>{setIsOpen(false)}
+
+  
   return (
+    
     <div className="flex font-poppins overflow-hidden bg-primary-darkPurple">
-        <SideMenu/>
+        <SideMenu openModal={openModal}/>
+        <ModalColumn isOpen={isOpen} onRequestClose={closeModal}/>
         <div className="flex flex-col w-screen">
           <Header /> 
           <div className="flex flex-row text-">
--- src/components/modalColumn.jsx
+++ src/components/modalColumn.jsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "react-modal"
 
-Modal.setAppElement("root");
+Modal.setAppElement('#root');
 
 const ModalColumn = ({isOpen,onRequestClose}) =>{
     
+     const [inputValue, setInputValue] = useState ('')
+
+    function valueInput (e){
+
+        e.preventDefault();
+        const value = e.target.title.value;
+        setInputValue(value);
+        console.log(value)
+    }
+    
     return(
+
        <Modal
+       className="rounded-xl w-[400px] min-h-max p-10 font-poppins text-white bg-primary-lightPurple" 
        isOpen={isOpen}
        onRequestClose={onRequestClose}
        contentLabel="Example Modal"
-       overlayClassName="Overlay"
+       overlayClassName="flex fixed inset-0 justify-center items-center bg-gray-900 bg-opacity-55"
        >
-        <h2>Hello World</h2>
-        <button onClick={onRequestClose}>Close Modal</button>
+        <h1 className="text-3xl mb-10">Add New Board</h1>
+        <div className="flex flex-col ">
+            <label className="text-2xl mb-2">Title</label>
+            <input type="text" name="title" placeholder="escreva um titulo"className=" p-4 h-12 rounded-lg bg-transparent border-[2px] border-gray-600"></input>
+            <label className="text-2xl mt-6 mb-2">Description</label>
+            <input type="text" placeholder="escreva uma descrição" className="p-4 min-h-40 rounded-lg bg-transparent border-[2px] border-gray-600 -"></input>
+        </div>
+        <button
+        className="bg-secondary-purple py-4 px-48 text-2xl rounded-full mt-10" 
+        onClick={onRequestClose}>Save</button>
        </Modal>
     )
 }
